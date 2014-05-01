9356241f3c5601596a24cf35da82169af00a2a81
Lee Bergstrand <leemacboy@gmail.com>
2014-04-30 20:33:56 -0700

    Added javascript file

---
+++ Hydrophobicity.js
@@ -0,0 +1,8 @@
+/***************************************\
+| Javascripts for Hydrobobicity         |
+| Date: 13-04-29                        |
+| Created by: Lee Bergstrand            |
+\***************************************/
+
+// Variables
+
