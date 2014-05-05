123ae0cf13931b263d6e7ab7267db79c8541bb3b
Lee Bergstrand <leemacboy@gmail.com>
2014-05-04 21:43:01 -0700

    Now reads from drop zone or file input box!

--- Hydrophobicity.js
+++ Hydrophobicity.js
@@ -8,29 +8,26 @@
 //===========================================================================================
 // Global Variables
 //===========================================================================================
-
-
-
-
+var FastaString = ""
+var FastaHeader = ""
+var FastaSeq    = ""
 
 //===========================================================================================
 // Funtions:
 //===========================================================================================
 function handleFileSelect(event) 
+{	
+	var file = event.target.files[0]; // FileList object.
+	parseFile(file);
+}
+//-------------------------------------------------------------------------------------------
+function handleDropzoneFileSelect(event) 
 {
 	event.stopPropagation();
 	event.preventDefault();
 	
 	var file = event.dataTransfer.files[0]; // FileList object.
-
-	var reader = new FileReader();
-	reader.onload = function(e) 
-	{
-		var text = reader.result;
-		alert(text)
-	}
-	reader.readAsText(file);
-	
+	parseFile(file);
 }
 //-------------------------------------------------------------------------------------------
 function handleDragOver(event) 
@@ -39,7 +36,31 @@ function handleDragOver(event)
 	event.preventDefault();
 	event.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
+//-------------------------------------------------------------------------------------------
+function parseFile(file) 
+{
+	var reader = new FileReader();
+	reader.onload = function(e) 
+	{
+		FastaString = reader.result;
+		alert(FastaString)
+		FastaExtraction(FastaString) 
+	}
+	reader.readAsText(file);
+}
+//-------------------------------------------------------------------------------------------
+function FastaExtraction(FastaString) 
+{
+	var FastaComponents = FastaString.split(/\n/g)
 
+	FastaHeader = FastaComponents[0]
+	console.log(FastaHeader)
+	for(var x = 1; x < FastaComponents.length; x++)
+	{
+		FastaSeq += FastaComponents[x];
+	}
+	console.log(FastaSeq)
+}
 //===========================================================================================
 // Onload Code
 //===========================================================================================
@@ -59,7 +80,7 @@ function onLoad()
 	
 	var dropZone = document.getElementById('dropZone');
 	dropZone.addEventListener('dragover', handleDragOver, false);
-	dropZone.addEventListener('drop', handleFileSelect, false);
+	dropZone.addEventListener('drop', handleDropzoneFileSelect, false);
 }
 
 
