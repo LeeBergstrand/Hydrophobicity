04101b440812f4dbbf27d1919af1069607b10c2d
Lee Bergstrand <leemacboy@gmail.com>
2014-05-04 19:58:44 -0700

    Updates to js and html

--- Hydrophobicity.js
+++ Hydrophobicity.js
@@ -1,7 +1,7 @@
 /*************************************************************************\
-| Javascript for Hydrobobicity                                           |
+| Javascript for Hydrobobicity                                            |
 | Date: 13-04-29                                                          |
-| Created by: Lee Bergstrand			                          |
+| Created by: Lee Bergstrand			                                  |
 | File IO code from http://www.html5rocks.com/en/tutorials/file/dndfiles/ |
 \*************************************************************************/
 
@@ -18,49 +18,18 @@
 //===========================================================================================
 // Funtions:
 //===========================================================================================
-
-function handleFileSelect(evt) 
+function handleFileSelect(event) 
 {
-    var files = evt.target.files; // FileList object
-
-    // files is a FileList of File objects. List some properties.
-    var output = [];
-    for (var i = 0, f; f = files[i]; i++)
-    {
-      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-                  f.size, ' bytes, last modified: ',
-                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-                  '</li>');
-    }
-    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+	event.stopPropagation();
+	event.preventDefault();
+	var files = event.dataTransfer.files; // FileList object.
 }
 //-------------------------------------------------------------------------------------------
-
-function handleFileSelect(evt) 
+function handleDragOver(event) 
 {
-	evt.stopPropagation();
-	evt.preventDefault();
-	
-	var files = evt.dataTransfer.files; // FileList object.
-	
-	// files is a FileList of File objects. List some properties.
-	var output = [];
-	for (var i = 0, f; f = files[i]; i++) 
-	{
-		output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-		          f.size, ' bytes, last modified: ',
-		          f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-		          '</li>');
-	}
-	document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-}
-//-------------------------------------------------------------------------------------------
-
-function handleDragOver(evt) 
-{
-	evt.stopPropagation();
-	evt.preventDefault();
-	evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+	event.stopPropagation();
+	event.preventDefault();
+	event.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
 
 //===========================================================================================
@@ -80,6 +49,6 @@ else
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
 // Setup the dnd listeners.
-var dropZone = document.getElementById('drop_zone');
+var dropZone = document.getElementById('dropZone');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
